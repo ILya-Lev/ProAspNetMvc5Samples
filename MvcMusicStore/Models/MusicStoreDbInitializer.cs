90e596f05f45fc18de0645388729e3decbf241e9
ILya-Lev <lis2889@gmail.com>
2017-11-19 00:30:12 +0200

    added seed db initializer

---
+++ MvcMusicStore/Models/MusicStoreDbInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+
+namespace MvcMusicStore.Models
+{
+	public class MusicStoreDbInitializer : DropCreateDatabaseAlways<MusicStoreDB>
+	{
+		protected override void Seed(MusicStoreDB context)
+		{
+			context.Artists.Add(new Artist { Name = "RHCP" });
+			context.Genres.Add(new Genre { Name = "Pop Rock" });
+			context.Albums.Add(new Album
+			{
+				Artist = new Artist { Name = "Ac/Dc" },
+				Genre = new Genre { Name = "Rock" },
+				Price = 99.9m,
+				Title = "Back in Black"
+			});
+
+			base.Seed(context);     //does nothing - is it required?
+		}
+	}
+}
