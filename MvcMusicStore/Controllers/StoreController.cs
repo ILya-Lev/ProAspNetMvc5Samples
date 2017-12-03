23ee2557687c254c460f24437b48d2a7ffba77c6
ILya-Lev <lis2889@gmail.com>
2017-12-04 00:47:39 +0200

    added sample Store/Index and Buy actions the later is decorated with Authorize attribute

--- MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web;
+﻿using MvcMusicStore.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 
 namespace MvcMusicStore.Controllers
@@ -6,9 +9,9 @@ namespace MvcMusicStore.Controllers
 	public class StoreController : Controller
 	{
 		// GET: Store
-		public string Index()
+		public ActionResult Index()
 		{
-			return "Hello from Store.Index";
+			return View(_staticStorage);
 		}
 
 		//GET: Store/Browse?genre=string
@@ -23,5 +26,20 @@ namespace MvcMusicStore.Controllers
 		{
 			return $"Store.Details, ID = {id}";
 		}
+
+		[Authorize]
+		public ActionResult Buy(int albumId)
+		{
+			var theAlbum = _staticStorage.Single(a => a.AlbumId == albumId);
+			return View(theAlbum);
+		}
+
+		private static readonly IReadOnlyList<Album> _staticStorage = new List<Album>
+		{
+			new Album { AlbumId = 2, Title = "The fall of math", Price = 8.99m},
+			new Album { AlbumId = 3, Title = "The blue notebooks", Price = 8.99m},
+			new Album { AlbumId = 4, Title = "Lost in translation", Price = 9.99m},
+			new Album { AlbumId = 5, Title = "Permutation", Price = 10.99m},
+		};
 	}
 }
